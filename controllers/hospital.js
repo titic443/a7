9f03ccd446477d04937321e3141bb3c4680e608a
titic443 <titi.changpoo@gmail.com>
2025-03-09 21:34:38 +0700

    Chapter08: setup pagination

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -1,12 +1,60 @@
 const Hospital = require('../models/Hospital');
 
 exports.getHospitals = async (req, res, next) => {
-    try {
-        const hospitals = await Hospital.find();
+    let query;
 
+    const reqQuery = { ...req.query };
+
+    const removeFields = ['select', 'sort', 'page', 'limit'];
+
+
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    console.log(reqQuery);
+
+
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+    console.log(queryStr)
+    query = Hospital.find(JSON.parse(queryStr));
+
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Hospital.countDocuments();
+    query = query.skip(startIndex).limit(limit);
+    try {
+        const hospitals = await query;
+        const pagination = {};
+
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit
+            };
+        }
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit
+            };
+        }
         res.status(200).json({
             success: true,
             count: hospitals.length,
+            // pagination,
             data: hospitals
         });
     } catch (err) {
@@ -38,10 +86,10 @@ exports.createHospital = async (req, res, next) => {
             success: true,
             data: hospital
         });
-    }catch(e) {
+    } catch (e) {
         res.status(400).json({ success: false });
     }
- 
+
 };
 
 
