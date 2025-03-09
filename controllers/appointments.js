902eac94307f989cf4690c6c68e242e3ae571ee1
titic443 <titi.changpoo@gmail.com>
2025-03-09 22:04:40 +0700

    Chapter08: setup populate

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -3,9 +3,9 @@ const Appointment = require('../models/Appointment');
 exports.getAppointments = async (req, res, next) => {
     let query;
     if (req.user.role !== 'admin') {
-        query = Appointment.find({ user: req.user.id });
+        query = Appointment.find({ user: req.user.id }).populate({ path: 'hospital', select: 'name province tel' });
     } else {
-        query = Appointment.find();
+        query = Appointment.find().select({ path: 'hospital', select: 'name province tel' });
     }
     try {
         const appointments = await query;
