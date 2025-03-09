72fbf2e25b4bbf2e4907fe87a507b4dc2af816cf
titic443 <titi.changpoo@gmail.com>
2025-03-09 23:18:37 +0700

    Chapter08: Add appointment

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,4 +1,5 @@
 const Appointment = require('../models/Appointment');
+const Hospital = require('../models/Hospital');
 
 exports.getAppointments = async (req, res, next) => {
     let query;
@@ -24,3 +25,32 @@ exports.getAppointments = async (req, res, next) => {
         return res.status(500).json({ success: false, message: "Cannot find Appointment" });
     }
 };
+
+exports.getAppointment = async (req, res, next) => {
+    try {
+        const appointment = await Appointment.findById(req.params.id).populate({ path: 'hospital', select: 'name description tel' });
+        if (!appointment) {
+            return res.status(400).json({ success: false, message: `No appointment with the id of ${req.params.id}` });
+        }
+        res.status(200).json({ success: true, data: appointment });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+}
+
+exports.addAppointment = async (req, res, next) => {
+    try {
+        req.body.hospital = req.params.hospitalId;
+        console.log(req.body)
+        const hospital = await Hospital.findById(req.params.hospitalId);
+        if (!hospital) {
+            return res.status(404).json({ success: false, message: `No hospital with the id of ${req.params.hospitalId}` });
+        }
+
+        const appointment = await Appointment.create(req.body);
+        res.status(200).json({ success: true, data: appointment });
+    } catch (err) {
+        // console.log(err)
+        res.status(500).json({ success: false, message: "Cannot create Appointment" });
+    }
+}
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { getAppointments } = require('../controllers/appointments');
+const { getAppointments, getAppointment, addAppointment } = require('../controllers/appointments');
 
 const router = express.Router({ mergeParams: true });
 
 const { protect } = require('../middleware/auth');
 
-router.route('/').get(protect, getAppointments);
+router.route('/').get(protect, getAppointments).post(protect, addAppointment)
+router.route('/:id').get(getAppointment);
+// router.route('/').get(protect, getAppointments);
+// router.route('/:id').get(protect, getAppointment);
 
 module.exports = router;
